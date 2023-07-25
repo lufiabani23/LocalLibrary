11ada8391a6a819c9e0b33e78b311ce2c93c9bba
lufiabani <luisfiabani@gmail.com>
2023-07-25 09:14:23 -0300

    Desafio de rotas realizado

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.get('/cool', function(req, res, next) {
+  res.send('Você é tão legal.');
+});
+
+router.get('/cool/beach', function(req, res, next) {
+  res.send('Adoro praia!');
+});
+
+router.get('/ifc', function(req, res, next) {
+  res.send('Bem-vindo ao IFC.');
+});
 module.exports = router;
